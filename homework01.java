e8403732af5fb06d7c46eef9793746cca384f607
Ans Hong Gil <qws458@naver.com>
2019-01-14 14:52:31 +0900

    테스트 완료

--- homework01.java
+++ homework01.java
@@ -1,14 +1,15 @@
 // Solution 함수를 작성하세요
-
+// 테스트 완료!!
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-
+        System.out.println("테스트 완료!");
         return answer;
     }
 }
 
 public class homework01 {
+		
 }
