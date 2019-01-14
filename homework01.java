fead2f149575c1a2416e800fa21e460910e8f00c
eojun <eojun23@gmail.com>
2019-01-14 14:43:10 +0900

    Add Homewrok01.java

---
+++ homework01.java
@@ -0,0 +1,14 @@
+// Solution 함수를 작성하세요
+
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+
+        return answer;
+    }
+}
+
+public class homework01 {
+}
