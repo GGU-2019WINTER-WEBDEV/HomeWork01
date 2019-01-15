f61403f7b439a67e18e474af0530b31a5fd2d65d
YU MINWOO <youmw99@naver.com>
2019-01-15 16:55:20 +0900

    셋과 맵(가볍게 개념만 숙지)

---
+++ Map_test.java
@@ -0,0 +1,15 @@
+
+import java.util.HashMap;
+
+public class Map_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		map.put("박순규", 0);
+		map.put("유민우", 30);
+		map.put("박은빛", 40);
+		map.put("안종길", 80);
+	}
+
+}
---
+++ Set_test.java
@@ -0,0 +1,14 @@
+
+import java.util.HashSet;
+
+public class Set_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HashSet<String> hashset = new HashSet<String>();
+		hashset.add("³È³È");
+		hashset.add("³È³È");
+		System.out.println(hashset);//Áßº¹°ª Çã¿ë ¾ÈÇÏ¹Ç·Î ÇÑ °³¸¸ ³ª¿È
+	}
+
+}
